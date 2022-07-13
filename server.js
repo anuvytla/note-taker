67d74c21cabbfe5151f0ccb8119db5cb27fe5c27
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-07-13 12:36:18 -0700

    initialize routes for get request

--- server.js
+++ server.js
@@ -5,10 +5,34 @@ const path = require('path');
 const PORT = 3001;
 
 const app = express();
+const notes = []
+
+function loadNotes() {
+  fs.readFile('/db/db.json','utf8', (err,data) => {
+    if(err) throw err;
+    notes = JSON.parse(data);
+  });
+}
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended : true}));
+app.use(express.json);
+
+app.get('/notes' , (req, res) => {
+  res.sendFile(path.join(__dirname , '/public/notes.html'));
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
 
-app.listen(PORT, () =>
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.listen(PORT, () => {
+  loadNotes();
   console.log(`App listening at http://localhost:${PORT} 🚀`)
+}
 );
