a2f3304d90b91789b4fc0bd3872db19434c4f295
Lakshmi Anusha Vytla <reachinganusha@gmail.com>
2022-07-14 12:50:11 -0700

    Implemented apis for creating and deleting notes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const PORT = 3001;
 
 const app = express();
-const notes = []
+let notes = []
 
 function loadNotes() {
-  fs.readFile('/db/db.json','utf8', (err,data) => {
+  fs.readFile('./db/db.json','utf8', (err,data) => {
     if(err) throw err;
     notes = JSON.parse(data);
   });
@@ -16,19 +16,52 @@ function loadNotes() {
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended : true}));
-app.use(express.json);
+app.use(express.json());
 
 app.get('/notes' , (req, res) => {
   res.sendFile(path.join(__dirname , '/public/notes.html'));
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get('/api/notes', (req, res) => {
+  return res.json(notes);
+});
+
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.post('/api/notes', (req,res) => {
+ 
+  let newNote = req.body;
+  notes.push(newNote);
+  updateNotes();
+  console.log("Added new note: "+newNote.title);
+
 });
 
 
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+
+function updateNotes() {
+  fs.writeFile("./db/db.json",JSON.stringify(notes,'\t'),err => {
+    if (err) {
+      return res.status(400).json({err});
+    }
+    res.json(notes);
+  });
+}
+
+
+
+app.get('/api/notes/:id' , (req, res) => {
+  res.json(notes[req.params.id]);
+});
+
+
+
+app.delete('/api/notes/:id' , (req,res) => {
+  notes.splice(req.params.id , 1);
+  updateNotes();
+  console.log('Deleted notes of id' + req.params.id);
 });
 
 app.listen(PORT, () => {
